cdf59b078ad67786c4ebf74e8810bac6d6112acb
侯淼 <houmiao@houmiaodeMacBook-Air.local>
2023-09-15 23:46:55 +0800

    Version 3 Succeed

--- TeamBook/Controller/SignUpViewController.swift
+++ TeamBook/Controller/SignUpViewController.swift
@@ -9,8 +9,24 @@ import UIKit
 import Realm
 import Firebase
 class SingUpViewController: UIViewController{
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    @IBAction func signUpButton(_ sender: UIButton) {
+        if let emialText = emailTextField.text, let passwordText = passwordTextField.text{
+            Auth.auth().createUser(withEmail: emialText, password: passwordText){ _ ,error in
+                if let e = error{print("123")}
+                else{
+                    print("321")
+                }
+            }
+        }
+        self.navigationController!.popToRootViewController(animated: true)
+    }
+    
     
 }
