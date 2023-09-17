47b83d3e881435d2fccf592109d4fe075f53284e
侯淼 <houmiao@houmiaodeMacBook-Air.local>
2023-09-18 00:24:28 +0800

    Version 10 Succeed

--- TeamBook/Controller/MainController1.swift
+++ TeamBook/Controller/MainController1.swift
@@ -6,15 +6,11 @@
 //
 
 import UIKit
-import Photos
-import Firebase
 import FirebaseStorage
-import FirebaseFirestore
-import FirebaseStorageUI
-
 
 class MainController1: UIViewController{
     @IBOutlet weak var imageView: UIImageView!
+    
     var imagePicker = UIImagePickerController()
     
     override func viewDidLoad() {
@@ -24,69 +20,50 @@ class MainController1: UIViewController{
     
     @IBAction func downloadImage(_ sender: UIButton) {
         let storage = Storage.storage()
-        let storageRef = storage.reference()
-        let ref = storageRef.child("UploadPhotoOne")
-        imageView.sd_setImage(with: ref)
-
-        
+        // 创建 StorageReference，指向要下载的图像
+        let storageRef = storage.reference().child("UploadPhotoOne")
+        // 下载图像
+        storageRef.getData(maxSize: 5 * 1024 * 1024) { [weak self] (data, error) in
+            if let error = error {
+                print("Error downloading image: \(error.localizedDescription)")
+            } else if let data = data {
+                // 将下载的数据转换为UIImage
+                if let image = UIImage(data: data) {
+                    // 在主线程上更新UI
+                    DispatchQueue.main.async {
+                        self?.imageView.image = image
+                    }
+                }
+            }
+        }
     }
+    
     @IBAction func addPhotoButton(_ sender: UIButton) {
-        
-        //imagePicker.sourceType = .photoLibrary
-        //present(imagePicker,animated: true,completion: nil)
-        self.imagePicker.sourceType = .photoLibrary
+        self.imagePicker.sourceType = .photoLibrary //sourceType表示打开imagePicker的方式，这个方式为photoLibrary，打开相册
         self.present(self.imagePicker, animated: true, completion:nil)
     }
     
-//    func uploadImageToFirebase(image: UIImage) {
-//        guard let imageData = image.jpegData(compressionQuality: 1.0) else { return }
-//
-//        let imageRef = storageRef.child("images123.jpg")
-//
-//        let metadata = StorageMetadata()
-//        metadata.contentType = "image/jpeg"
-//
-//        imageRef.putData(imageData, metadata: metadata) { (metadata, error) in
-//            if let error = error {
-//                print("上传错误: \(error.localizedDescription)")
-//            } else {
-//                print("Image uploaded successfully!")
-//                // 在metadata中可以获取上传后的文件信息，例如下载URL
-////                if let downloadURL = metadata?.downloadURL()?.absoluteString {
-////                    print("Download URL: \(downloadURL)")
-////                }
-//            }
-//        }
-//    }
-    
-    func requsetAuthroizationHandler(status:PHAuthorizationStatus){
-        if PHPhotoLibrary.authorizationStatus() == PHAuthorizationStatus.authorized{
-            print("1")
-        }else{
-            print("2")
-        }
-    }
-    
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+    //选择一张照片后这个函数被触发
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {//如果能从info中获取到url，就uploadToCloud
         if let url = info[UIImagePickerController.InfoKey.imageURL] as? URL{
-            print(url)
+            print("func imagePickerController 函数得到的url：\(url)")
             uploadToCloud(fileURL: url)
         }
-        imagePicker.dismiss(animated: true,completion: nil)
+        imagePicker.dismiss(animated: true,completion: nil)//关闭imagePicker
     }
     
     func uploadToCloud(fileURL:URL){
         let storage = Storage.storage()
-        let data = Data()
+        //let data = Data()
         let storageRef = storage.reference()
         let localFile = fileURL
         let photoRef = storageRef.child("UploadPhotoOne")
         let uploadTesk = photoRef.putFile(from: localFile, metadata: nil) { (metedata,error) in
-            guard let metedata = metedata else {
-                print(error)
+            guard metedata != nil else {
+                print("func uploadToCloud error!!!")
                 return
             }
-            print("11111111111")
+            print("func uploadToCloud")
         }
     }
 
@@ -94,43 +71,6 @@ class MainController1: UIViewController{
 
 
 extension MainController1: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-    
-//    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-//        // 当用户选择或拍摄图像后调用此方法
-//        if let pickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
-//            // 处理所选图像
-//            uploadImageToFirebase(image: pickedImage)
-//        }
-//
-//        picker.dismiss(animated: true, completion: nil) // 关闭UIImagePickerController
-//    }
-    
-//    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-//        // 用户取消选择或拍摄时调用此方法
-//        picker.dismiss(animated: true, completion: nil) // 关闭UIImagePickerController
-//    }
-    
-//    func uploadImageToFirebase(image: UIImage) {
-//        guard let imageData = image.jpegData(compressionQuality: 1.0) else { return }
-//
-//        let imageRef = storageRef.child("images/\(UUID().uuidString).jpg")
-//
-//        let metadata = StorageMetadata()
-//        metadata.contentType = "image/jpeg"
-//
-//        imageRef.putData(imageData, metadata: metadata) { (metadata, error) in
-//            if let error = error {
-//                print("Error uploading image: \(error.localizedDescription)")
-//            } else {
-//                print("Image uploaded successfully!")
-//                // 在metadata中可以获取上传后的文件信息，例如下载URL
-//                if let downloadURL = metadata?.downloadURL()?.absoluteString {
-//                    print("Download URL: \(downloadURL)")
-//                }
-//            }
-//        }
-//    }
-    
 
 }
 
