5a518784829bc09cb83ba073b1b39137463835a0
侯淼 <houmiao@houmiaodeMacBook-Air.local>
2023-09-20 21:22:15 +0800

    Version 19.0 Firebase Succeed but Realm not

--- TeamBook/Controller/TableController0.swift
+++ TeamBook/Controller/TableController0.swift
@@ -6,18 +6,18 @@
 //
 
 import UIKit
-import RealmSwift
+//import RealmSwift
 import Firebase
-import FirebaseFirestore
+//import FirebaseFirestore
 
 class TableController0: UITableViewController {
     
-    let realm = try! Realm()
-    var teams : Results<Team>?
-    var data = [String]()           //为了realm
-    var teamsSelection: [TeamsSelection] = []
+    //let realm = try! Realm()
+    //var teams : Results<Team>?
+    //var data = [String]()
+    // var teamsSelection: [TeamsSelection] = []
     let db = Firestore.firestore()
-    var dataArray = [String]()      //为了firebase
+    var FirebaseDataArray = [String]()
 
     
     
@@ -25,21 +25,21 @@ class TableController0: UITableViewController {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
-        teams = realm.objects(Team.self) // 假设 Item 是您的 Realm 数据库模型类
-        tableView.reloadData()
+        //teams = realm.objects(Team.self) // 假设 Item 是您的 Realm 数据库模型类
         let longPressGesture = UILongPressGestureRecognizer(target: self, action: #selector(handleLongPress(_:)))
         tableView.addGestureRecognizer(longPressGesture)
         downloadFromFirebase()
+        tableView.reloadData()
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         //return data.count
-        return dataArray.count
+        return FirebaseDataArray.count
         
     }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell",for: indexPath)
-        cell.textLabel?.text = dataArray[indexPath.row]
+        cell.textLabel?.text = FirebaseDataArray[indexPath.row]
         return cell
     }
     
@@ -68,28 +68,28 @@ class TableController0: UITableViewController {
 
     func deleteItem(at indexPath: IndexPath) {
         // 获取要删除的对象，假设您的数据源是 Realm 中的 Results<Item>
-        if let teamToDelete = teams?[indexPath.row] {
-            do {
-                // 在写事务中执行删除操作
-                try realm.write {
-                    realm.delete(teamToDelete)
-                }
-                
-                // 更新数据源以移除被删除的对象
-                // 假设 items 是 Results<Item>，需要刷新它
-                teams = realm.objects(Team.self)
-                
-                // 更新表视图以反映删除
-                tableView.deleteRows(at: [indexPath], with: .fade)
-            } catch {
-                print("Error deleting item from Realm: \(error.localizedDescription)")
-            }
-        }
+//        if let teamToDelete = teams?[indexPath.row] {
+//            do {
+//                // 在写事务中执行删除操作
+//                try realm.write {
+//                    realm.delete(teamToDelete)
+//                }
+//
+//                // 更新数据源以移除被删除的对象
+//                // 假设 items 是 Results<Item>，需要刷新它
+//                teams = realm.objects(Team.self)
+//
+//                // 更新表视图以反映删除
+//                tableView.deleteRows(at: [indexPath], with: .fade)
+//            } catch {
+//                print("Error deleting item from Realm: \(error.localizedDescription)")
+//            }
+//        }
     }
 
     func sendToFirebase(with whichTeam: String){
-        let collectionRef = db.collection("your_collection_name") // 替换为您的集合名称
-        collectionRef.addDocument(data: ["teamSelection":whichTeam]) { (error) in
+        let collectionRef = db.collection("collectionName") // 替换为您的集合名称
+        collectionRef.addDocument(data: ["teamSelection":whichTeam,"time":Date().timeIntervalSince1970]) { (error) in
                if let error = error {
                    print("Error saving data to Firestore: \(error.localizedDescription)")
                } else {
@@ -99,24 +99,25 @@ class TableController0: UITableViewController {
     }
     
     func downloadFromFirebase(){
-        let collectionRef = db.collection("your_collection_name") // 替换为您的集合名称
-
+        let collectionRef = db.collection("collectionName").order(by: "time")// 替换为您的集合名称
+        self.FirebaseDataArray = []
         collectionRef.getDocuments { (querySnapshot, error) in
                 if let error = error {
                     print("Error retrieving data from Firestore: \(error.localizedDescription)")
                     return
                 }
-
                 // 处理查询结果并将数据存储到 dataArray 中
                 if let documents = querySnapshot?.documents {
-                    for document in documents {
-                        if let data = document.data()["teamSelection"] as? String {
-                            self.dataArray.append(data)
+                    for doc in documents {
+                        if let data = doc.data()["teamSelection"] as? String {
+                            self.FirebaseDataArray.append(data)
+                            DispatchQueue.main.async{
+                                self.tableView.reloadData()
+                                let indexPath = IndexPath(row: self.FirebaseDataArray.count - 1, section: 0)
+                                self.tableView.scrollToRow(at:indexPath, at:.top, animated: true)
+                            }
                         }
                     }
-
-                    // 刷新表格视图
-                    self.tableView.reloadData()
                 }
             }
     }
@@ -133,15 +134,18 @@ class TableController0: UITableViewController {
         let confirmAction = UIAlertAction(title: "Add", style: .default) { (_) in
             if let text = alertController.textFields?.first?.text {
                 do {
-                    try self.realm.write {
-                        let newTeam = Team()
-                        newTeam.teamName = text
-                        self.realm.add(newTeam)
-                    }
+//                    try self.realm.write {
+//                        let newTeam = Team()
+//                        newTeam.teamName = text
+//                        self.realm.add(newTeam)
+//                    }
                     // 更新数据源
                     self.sendToFirebase(with: text)
-                    self.teams = self.realm.objects(Team.self)
-                    self.tableView.reloadData()
+                   // self.teams = self.realm.objects(Team.self)
+                    DispatchQueue.main.async {
+                        self.downloadFromFirebase()
+                        self.tableView.reloadData()
+                    }
                 } catch {
                     print("Error saving data to Realm: \(error.localizedDescription)")
                 }
@@ -157,12 +161,11 @@ class TableController0: UITableViewController {
         
         // 弹出 UIAlertController
         present(alertController, animated: true, completion: nil)
-        
-        
-        if let realmURL = Realm.Configuration.defaultConfiguration.fileURL {
-            print("Realm file location: \(realmURL)")
-        }
-        
+//        downloadFromFirebase()
+//        tableView.reloadData()
+//        if let realmURL = Realm.Configuration.defaultConfiguration.fileURL {
+//            print("Realm file location: \(realmURL)")
+//        }
     }
 
 }
--- TeamBook/Model/Team.swift
+++ TeamBook/Model/Team.swift
@@ -6,10 +6,10 @@
 //
 
 import Foundation
-import RealmSwift
-class Team: Object {
-    @objc dynamic var teamName: String = ""
-}
+//import RealmSwift
+//class Team: Object {
+//    @objc dynamic var teamName: String = ""
+//}
 struct TeamsSelection{
     let whichTeam: String
 }
