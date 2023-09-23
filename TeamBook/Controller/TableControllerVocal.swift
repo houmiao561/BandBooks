ebd8665d07f437461a8d1599a27003140e879234
侯淼 <houmiao@houmiaodeMacBook-Air.local>
2023-09-24 00:57:52 +0800

    Version 29 Succeed

--- TeamBook/Controller/TableControllerVocal.swift
+++ TeamBook/Controller/TableControllerVocal.swift
@@ -14,6 +14,8 @@ class TableControllerVocal: UITableViewController {
     private var firebaseDataArray = [FirebaseDataArray]()
     private let user = Auth.auth().currentUser
     var buttonName: String = ""
+    var allEmailText = [String]()
+    var selectCellEmail = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self
@@ -58,6 +60,7 @@ extension TableControllerVocal {
     func downloadFromFirebase(){
         let collectionRef = db.collection("collection:\(buttonName)").order(by: "sendTime")// 替换为您的集合名称
         self.firebaseDataArray = []
+        self.allEmailText = []
         collectionRef.getDocuments { (querySnapshot, error) in
             if let error = error {
                 print("Error retrieving data from Firestore: \(error.localizedDescription)")
@@ -69,13 +72,16 @@ extension TableControllerVocal {
                     if let name = doc.data()["Name"]as? String,
                        let location = doc.data()["Location"]as? String,
                        let musicStyle = doc.data()["MusicStyle"]as? String,
-                       let selfIntroduction = doc.data()["SelfIntroduction"]as? String
+                       let selfIntroduction = doc.data()["SelfIntroduction"]as? String,
+                       let someoneName = doc.data()["someoneName"]as? String
                     {
+                        
                         let balabala = FirebaseDataArray(name: name,
                                                          musicStyle: musicStyle,
                                                          location: location,
                                                          selfIntroduction: selfIntroduction)
                         self.firebaseDataArray.append(balabala)
+                        self.allEmailText.append(someoneName)
                         DispatchQueue.main.async{
                             self.tableView.reloadData()
                             let indexPath = IndexPath(row: self.firebaseDataArray.count - 1, section: 0)
@@ -132,52 +138,54 @@ extension TableControllerVocal {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
+        //selectCellEmail = indexPath.row
     }
     
     @objc func handleLongPress(_ gestureRecognizer: UILongPressGestureRecognizer) {
-        if gestureRecognizer.state == .began {
-            let collectionRef = db.collection("collection:\(buttonName)")
-            collectionRef.getDocuments { (querySnapshot, error) in
-                if let documents = querySnapshot?.documents{
-                    for doc in documents{
-                        if let selfIntroduction = doc.data()["SelfIntroduction"]as? String,
-                           let someoneName = doc.data()["someoneName"]as? String{
-                            
-                            
-                            if self.user!.email == someoneName{
-                                let point = gestureRecognizer.location(in: self.tableView)
-                                if let indexPath = self.tableView.indexPathForRow(at: point) {
-                                    let alertController = UIAlertController(title: "Delete Item",message: "Are you sure you want to delete this item?",preferredStyle: .alert)
-                                    let deleteAction = UIAlertAction(title: "Delete", style: .destructive) { (_) in
-                                        let ToDelete = selfIntroduction
-                                        self.deleteFromFirebase(SelfIntroduction: ToDelete)
-                                        self.firebaseDataArray.remove(at: indexPath.row)
-                                        print(ToDelete)
-                                        self.tableView.reloadData()
-                                    }
-                                    let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        selectCellEmail = 0
+        let point = gestureRecognizer.location(in: self.tableView)
+        if let indexPath = self.tableView.indexPathForRow(at: point) {
+            selectCellEmail = indexPath.row
+            if gestureRecognizer.state == .began {
+                let collectionRef = db.collection("collection:\(buttonName)")
+                collectionRef.getDocuments { (querySnapshot, error) in
+                    if let documents = querySnapshot?.documents{
+                        for doc in documents{
+                            if let selfIntroduction = doc.data()["SelfIntroduction"]as? String{
+                             //  ,let someoneName = doc.data()["someoneName"]as? String
+                                if self.user!.email == self.allEmailText[self.selectCellEmail]{
+                                        let alertController = UIAlertController(title: "Delete Item",message: "Are you sure you want to delete this item?",preferredStyle: .alert)
+                                        let deleteAction = UIAlertAction(title: "Delete", style: .destructive) { (_) in
+                                            let ToDelete = selfIntroduction
+                                            self.deleteFromFirebase(SelfIntroduction: ToDelete)
+                                            self.firebaseDataArray.remove(at: indexPath.row)
+                                            print(ToDelete)
+                                            self.tableView.reloadData()
+                                        }
+                                        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+                                        
+                                        alertController.addAction(deleteAction)
+                                        alertController.addAction(cancelAction)
+                                        
+                                        self.present(alertController, animated: true, completion: nil)
                                     
-                                    alertController.addAction(deleteAction)
-                                    alertController.addAction(cancelAction)
-                                    
-                                    self.present(alertController, animated: true, completion: nil)
                                 }
-                            }
-                            
-                            
-                            else{
-                                let alertController = UIAlertController(title: "What?!", message: "You want to delete others message?", preferredStyle: .alert)
-                                let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
-                                alertController.addAction(cancelAction)
-                                self.present(alertController,animated: true,completion: nil)
+                                
+                                
+                                else{
+                                    let alertController = UIAlertController(title: "What?!", message: "You want to delete others message?", preferredStyle: .alert)
+                                    let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+                                    alertController.addAction(cancelAction)
+                                    self.present(alertController,animated: true,completion: nil)
+                                }
                             }
                         }
                     }
                 }
+                
+                
             }
-            
-            
-        }
+        }//if let indexPath = self.tableView.indexPathForRow(at: point)
     }
 }
 
