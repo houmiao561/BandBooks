54003caabb9b78de1461ebe9f350aad31d1cd325
侯淼 <houmiao@houmiaodeMacBook-Air.local>
2023-09-30 23:20:53 +0800

    Version 54 Succeed

--- TeamBook/Controller/LogInViewController.swift
+++ TeamBook/Controller/LogInViewController.swift
@@ -12,8 +12,8 @@ import FirebaseAuth
 class LogInController: UIViewController{
     @IBOutlet weak var LogInEmailTextField: UITextField!
     @IBOutlet weak var LogInPasswordTextField: UITextField!
-    var logInEmailText = "114@hm.com"
-    var logInPasswordText = "123456"
+    var logInEmailText = ""
+    var logInPasswordText = ""
     override func viewDidLoad() {
         super.viewDidLoad()
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
@@ -25,8 +25,16 @@ class LogInController: UIViewController{
     }
     
     @IBAction func logInButton(_ sender: UIButton) {
-        Auth.auth().signIn(withEmail: LogInEmailTextField.text!, password: LogInPasswordTextField.text!)
-        performSegue(withIdentifier: "LogInToMain0", sender: self)
+        Auth.auth().signIn(withEmail: LogInEmailTextField.text!, password: LogInPasswordTextField.text!) { (authResult, error) in
+            if let _ = error {
+                let alertController = UIAlertController(title: "Something Wrong !", message: "Plz check your email and password.", preferredStyle: .alert)
+                let cancelAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                alertController.addAction(cancelAction)
+                self.present(alertController,animated: true,completion: nil)
+            } else {
+                self.performSegue(withIdentifier: "LogInToMain0", sender: self)
+            }
+        }
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
--- TeamBook/Controller/SignUpViewController.swift
+++ TeamBook/Controller/SignUpViewController.swift
@@ -36,8 +36,16 @@ class SingUpViewController: UIViewController{
     }
     
     @IBAction func signUpButton(_ sender: UIButton) {
-        Auth.auth().createUser(withEmail: emailTextField.text!, password: passwordTextField.text!)
-        performSegue(withIdentifier: "SignUpToLogIn", sender: self)
+        Auth.auth().createUser(withEmail: emailTextField.text!, password: passwordTextField.text!) { (authResult, error) in
+            if let _ = error {
+                let alertController = UIAlertController(title: "Something Wrong !", message: "1.Plz enter your right email.\n2.The password must be 6 string long or more.", preferredStyle: .alert)
+                let cancelAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                alertController.addAction(cancelAction)
+                self.present(alertController,animated: true,completion: nil)
+            } else {
+                self.performSegue(withIdentifier: "SignUpToLogIn", sender: self)
+            }
+        }
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
