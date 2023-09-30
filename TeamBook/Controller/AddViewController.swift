6d74e8eff3557c85c69608a3802dd01347680ffb
侯淼 <houmiao@houmiaodeMacBook-Air.local>
2023-09-30 10:44:18 +0800

    Version 51 Succeed

--- TeamBook/Controller/AddViewController.swift
+++ TeamBook/Controller/AddViewController.swift
@@ -20,14 +20,32 @@ class AddViewController: UIViewController {
     @IBOutlet weak var MusicStyleText: UITextField!
     @IBOutlet weak var SelfIntroductionText: UITextView!
     
+    @IBOutlet weak var scrollView: UIScrollView!
     override func viewDidLoad() {
         super.viewDidLoad()
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
         view.addGestureRecognizer(tapGesture)
         let swipeDownGesture = UIPanGestureRecognizer(target: self, action: #selector(handleSwipeDown(_:)))
         view.addGestureRecognizer(swipeDownGesture)
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillHideNotification, object: nil)
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
     }
-    
+    @objc func adjustForKeyboard(notification: Notification) {
+        guard let keyboardValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
+
+        let keyboardScreenEndFrame = keyboardValue.cgRectValue
+        let keyboardViewEndFrame = view.convert(keyboardScreenEndFrame, from: view.window)
+
+        if notification.name == UIResponder.keyboardWillHideNotification {
+            SelfIntroductionText.contentInset = .zero
+        } else {
+            SelfIntroductionText.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardViewEndFrame.height - view.safeAreaInsets.bottom+15, right: 0)
+        }
+        SelfIntroductionText.scrollIndicatorInsets = SelfIntroductionText.contentInset
+        let selectedRange = SelfIntroductionText.selectedRange
+        SelfIntroductionText.scrollRangeToVisible(selectedRange)
+    }
     func sendToFirebase(){
         let collectionRef = db.collection("collection:\(buttonName)") // 替换为您的集合名称
         collectionRef.addDocument(data: ["Name":self.NameText!.text!,
